2da6556b844d98f41bee193fbab1efc6665a2671
sashi <satoshi3104.nishi@gmail.com>
2025-04-08 17:29:04 +0900

    インクルードパスの設定

--- WindowsDesktop/AsbindGenerator/Program.cs
+++ WindowsDesktop/AsbindGenerator/Program.cs
@@ -9,13 +9,35 @@ internal static class Program
         var currentDirectory = Environment.CurrentDirectory;
         Console.WriteLine(currentDirectory);
 
+        var repositoryRoot = Utils.FindAncestorDirectory(currentDirectory, "OpenSiv3D");
+        if (repositoryRoot == null)
+        {
+            Console.WriteLine("Could not find the OpenSiv3D repository root.");
+            return;
+        }
+
+        var someFile = Path.Combine(repositoryRoot, "Siv3D/include/Siv3D/Texture.hpp");
+        var content = File.ReadAllText(someFile);
+
+        var parseOption = new CppParserOptions().ConfigureForWindowsMsvc();
+        parseOption.IncludeFolders.AddRange([
+            Path.Combine(repositoryRoot, "Siv3D/include"),
+            Path.Combine(repositoryRoot, "Siv3D/include/ThirdParty"),
+            Path.Combine(repositoryRoot, "Siv3D/src"),
+            Path.Combine(repositoryRoot, "Siv3D/src/Siv3D-Platform/WindowsDesktop"),
+            Path.Combine(repositoryRoot, "Siv3D/src/Siv3D-Platform/OpenGL4"),
+            Path.Combine(repositoryRoot, "Siv3D/src/ThirdParty"),
+            Path.Combine(repositoryRoot, "Siv3D/src/ThirdParty/freetype"),
+            Path.Combine(repositoryRoot, "Siv3D/src/ThirdParty/skia"),
+            Path.Combine(repositoryRoot, "Siv3D/src/ThirdParty/soloud/include"),
+            Path.Combine(repositoryRoot, "Siv3D/src/ThirdParty-prebuilt"),
+            Path.Combine(repositoryRoot, "Dependencies/boost_1_83_0"),
+
+            Path.Combine(repositoryRoot, "Siv3D/include/Siv3D"), // ?
+        ]);
+
         // Parse a C++ files
-        var compilation = CppParser.Parse(@"
-            enum MyEnum { MyEnum_0, MyEnum_1 };
-            void function0(int a, int b);
-            struct MyStruct { int field0; int field1;};
-            typedef MyStruct* MyStructPtr;
-        ");
+        var compilation = CppParser.Parse(content, parseOption);
 
         // Print diagnostic messages
         foreach (var message in compilation.Diagnostics.Messages)
